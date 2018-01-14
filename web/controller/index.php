a145ac85db69d900f890458060f292be2bd02fec
longxinhong <30366837+longxinhong@users.noreply.github.com>
2018-01-14 16:49:21 +0800

    创建控制器
    创建控制器

---
+++ web/controller/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/14 0014
+ * Time: 16:47
+ */
+namespace web\controller;
+class Index{
+    public function show(){
+        echo  'my web';
+    }
+}
